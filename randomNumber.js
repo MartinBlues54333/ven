9a119b5f7609a7f10ee7be46c2733a40adfe51f3
MartinBlues54333 <162550881+MartinBlues54333@users.noreply.github.com>
2024-03-13 02:36:36 +0100

    Delete randomNumber.js

--- randomNumber.js
+++
@@ -1,9 +0,0 @@
-// Generate a random number between 1 and 10
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-
-// Check if the number is even or odd
-if (randomNumber % 2 === 0) {
-    console.log(randomNumber + " is even.");
-} else {
-    console.log(randomNumber + " is odd.");
-}
