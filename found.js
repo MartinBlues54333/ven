d87a34744362c85ce43acb652da39ceede5cc8b4
MartinBlues54333 <162550881+MartinBlues54333@users.noreply.github.com>
2024-04-07 05:14:40 +0200

    Create found.js

---
+++ found.js
@@ -0,0 +1,11 @@
+public class StringIndexOf {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int index = str.indexOf("World");
+        if (index != -1) {
+            System.out.println("Substring 'World' found at index " + index);
+        } else {
+            System.out.println("Substring 'World' not found.");
+        }
+    }
+}
