7aa594a17b6bd239b27dce63d9107a577c1dc6c2
MartinBlues54333 <162550881+MartinBlues54333@users.noreply.github.com>
2024-04-20 04:45:25 +0200

    Create startsWithHellodsfsd.js

---
+++ startsWithHellodsfsd.js
@@ -0,0 +1,7 @@
+public class StartsWithExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        boolean startsWithHello = str.startsWith("Hello");
+        System.out.println("Starts with 'Hello': " + startsWithHello);
+    }
+}
